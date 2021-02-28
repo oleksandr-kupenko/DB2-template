c1476498e612f6767178166c01a1ac6b51aa4a02
sandron <xoxol4uk22@gmail.com>
2021-02-28 21:36:22 +0200

    add ejs for opportunity include html

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const path = require('path');
 const app = express();
 const reload = require('reload');
 
-app.use(express.static(path.join(__dirname, '/src')));
-
 const port = process.env.PORT || 3001;
-app.set('port', port);
+
+app.use(express.static(path.join(__dirname, '/src')));
+app.set('views', './src');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/pages/*', function (req, res) {
+  res.render('pages/sign-in');
+});
+
+app.set('view engine', 'ejs');
+
+app.engine('html', require('ejs').renderFile);
 
 const server = http.createServer(app);
 
